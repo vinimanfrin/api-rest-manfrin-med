5d939617e3310a678d466204f424260b0230bfc7
vinimanfrin <viniciusmanfrin2019@gmail.com>
2024-01-22 13:01:14 -0300

    nova migration para adicionar a coluna "telefone" para os médicos

--- src/main/java/med/manfrin/api/dtos/medico/DadosCadastroMedico.java
+++ src/main/java/med/manfrin/api/dtos/medico/DadosCadastroMedico.java
@@ -15,6 +15,9 @@ public record DadosCadastroMedico(
         @NotBlank @Email
         String email,
 
+        @NotBlank
+        String telefone,
+
         @NotBlank @Pattern(regexp = "\\d{4,6}") //expressão regular , são de 4 a 6 digitos
         String crm,
 
