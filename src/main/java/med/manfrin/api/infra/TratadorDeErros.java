15b484f2116912418057a713b18c53af99db01ca
vinimanfrin <viniciusmanfrin2019@gmail.com>
2024-01-23 12:26:35 -0300

    tratamento de erros

---
+++ src/main/java/med/manfrin/api/infra/TratadorDeErros.java
@@ -0,0 +1,38 @@
+package med.manfrin.api.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class TratadorDeErros {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarErro404(){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class) //bean validation lança esse erro quando tem algum argumento inválido
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException ex){
+        var erros = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDataIntegrityViolation(DataIntegrityViolationException ex) {
+        var erros = ex.getMessage();
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(erros);
+    }
+
+    private record DadosErroValidacao(String campo, String mensagem){
+
+        public DadosErroValidacao(FieldError error){
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
+}
